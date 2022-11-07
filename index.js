fb3c42545a717cdeebd655c4902cc346cabc3c8c
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-11-07 00:57:27 +0000

    index: add test code for controller

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import Controller from './controller.js';
 import { CONTROLLER_TARGET, ROOT } from './env.js';
 const controller = new Controller(CONTROLLER_TARGET);
 const unlock = await controller.getLock();
+[1, 2, 3].map(i => controller.getLock(i).then(ul => {
+	console.log(`${i}th lock resolved`);
+	ul()
+}))
 console.log('====== DEVICE ACTIVE ======');
 await controller
+	.WAIT(0)
 	.LED(1).PWM(64)
 	.WAIT(1000)
 	.RST()
@@ -11,4 +16,5 @@ await controller
 console.log('====== COMMAND LOADED ======');
 await controller.exec()
 console.log('====== DONE ======');
-process.exit(0);
+unlock();
+setImmediate(() => process.exit(0));
